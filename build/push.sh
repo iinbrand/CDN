22f7b6d353cb8740daffc41be0ba3a75825a2c10
Fromz Prajuktar <pkfromz@gmail.com>
2016-01-21 23:22:48 +0700

    Fix subtree

--- build/push.sh
+++ build/push.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 #
 git push origin gh-pages
-git subtree push --prefix=img git@github.com:iinbrand/artwork.git master --squash
+#git subtree push --prefix=img git@github.com:iinbrand/artwork.git master --squash
